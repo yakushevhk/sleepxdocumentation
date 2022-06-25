b28b4277089b9cef5a716b263412b4e257a2c096
Hermann Yakushev <78789455+yakushevhk@users.noreply.github.com>
2022-06-25 04:53:31 +0300

    some cdn

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e8d073f8950b638"; }
+/******/ 		__webpack_require__.h = function() { return "a4b65f6a2e5a4103"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
